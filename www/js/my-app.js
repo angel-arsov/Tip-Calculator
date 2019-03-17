bee358f27344f41017c4c40660b64f267e13b296
angel-arsov <angel.g.arsov@gmail.com>
2019-03-17 20:04:47 +0200

    Add first radio buttons

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,11 +1,11 @@
 // Initialize app
-var myApp = new Framework7()
+let myApp = new Framework7()
 
 // If we need to use custom DOM library, let's save it to $$ variable:
-var $$ = Dom7
+let $$ = Dom7
 
 // Add view
-var mainView = myApp.addView('.view-main', {
+let mainView = myApp.addView('.view-main', {
   // Because we want to use dynamic navbar, we need to enable it for this view:
   dynamicNavbar: true
 })
@@ -15,7 +15,36 @@ $$(document).on('deviceready', function () {
   console.log('Device is ready!')
 })
 
-// Functions in page
+// Handle refresh button
 $$('#refresh').on('click', function () {
   console.log('Refresh clicked')
+})
+
+// Handle first group of radio buttons
+let detectTap = false
+$$(document).on('touchstart', function () {
+  detectTap = true // detects all touch events
+})
+$$(document).on('touchmove', function () {
+  detectTap = false // Excludes the scroll events from touch events
+})
+$$(document).on('click touchend', function (event) {
+  if (event.type === 'click') detectTap = true // detects click events
+  if (detectTap) {
+    if ($$('#smile-1').prop('checked')) {
+      $$('.fa-meh-rolling-eyes').css('color', '#bfcd3a')
+      $$('.fa-smile').css('color', '#8e8e93')
+      $$('.fa-grin-stars').css('color', '#8e8e93')
+    }
+    if ($$('#smile-2').prop('checked')) {
+      $$('.fa-smile').css('color', '#bfcd3a')
+      $$('.fa-meh-rolling-eyes').css('color', '#8e8e93')
+      $$('.fa-grin-stars').css('color', '#8e8e93')
+    }
+    if ($$('#smile-3').prop('checked')) {
+      $$('.fa-grin-stars').css('color', '#bfcd3a')
+      $$('.fa-smile').css('color', '#8e8e93')
+      $$('.fa-meh-rolling-eyes').css('color', '#8e8e93')
+    }
+  }
 })
