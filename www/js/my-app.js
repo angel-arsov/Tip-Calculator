c89f26563470b922cbb5b7265b62be8b014728e8
angel-arsov <angel.g.arsov@gmail.com>
2019-04-02 20:29:20 +0300

    Starting the calculating algorithm

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -67,40 +67,6 @@ $$(document).on('touchmove', function () {
 $$(document).on('click touchend', function (event) {
   if (event.type === 'click') detectTap = true // detects click events
   if (detectTap) {
-    // First group of radio buttons
-    if ($$('#service-1').prop('checked')) {
-      $$('.fa-mug-hot').css('color', '#bfcd3a')
-      $$('.fa-hamburger').css('color', '#8e8e93')
-      $$('.fa-glass-cheers').css('color', '#8e8e93')
-    }
-    if ($$('#service-2').prop('checked')) {
-      $$('.fa-hamburger').css('color', '#bfcd3a')
-      $$('.fa-mug-hot').css('color', '#8e8e93')
-      $$('.fa-glass-cheers').css('color', '#8e8e93')
-    }
-    if ($$('#service-3').prop('checked')) {
-      $$('.fa-glass-cheers').css('color', '#bfcd3a')
-      $$('.fa-hamburger').css('color', '#8e8e93')
-      $$('.fa-mug-hot').css('color', '#8e8e93')
-    }
-
-    // Second group of radio buttons
-    if ($$('#smile-1').prop('checked')) {
-      $$('.fa-meh-rolling-eyes').css('color', '#bfcd3a')
-      $$('.fa-smile').css('color', '#8e8e93')
-      $$('.fa-grin-stars').css('color', '#8e8e93')
-    }
-    if ($$('#smile-2').prop('checked')) {
-      $$('.fa-smile').css('color', '#bfcd3a')
-      $$('.fa-meh-rolling-eyes').css('color', '#8e8e93')
-      $$('.fa-grin-stars').css('color', '#8e8e93')
-    }
-    if ($$('#smile-3').prop('checked')) {
-      $$('.fa-grin-stars').css('color', '#bfcd3a')
-      $$('.fa-smile').css('color', '#8e8e93')
-      $$('.fa-meh-rolling-eyes').css('color', '#8e8e93')
-    }
-
     // Bill button
     $$('#bill-btn').on('click', function () {
       let billInput = $$('#bill-input').prop('value')
@@ -116,5 +82,55 @@ $$(document).on('click touchend', function (event) {
         $$('.popover').remove()
       }, 100)
     })
+
+    // Inititating the variables for the calculating algorithm
+    let bill = Number(dynamicPopover.text().replace(/^\D+/g, ''))
+    let service = 0
+    let smile = 0
+
+    // First group of radio buttons
+    if ($$('#service-1').prop('checked')) {
+      $$('.fa-mug-hot').css('color', '#bfcd3a')
+      $$('.fa-hamburger').css('color', '#8e8e93')
+      $$('.fa-glass-cheers').css('color', '#8e8e93')
+      service = 1
+    }
+    if ($$('#service-2').prop('checked')) {
+      $$('.fa-hamburger').css('color', '#bfcd3a')
+      $$('.fa-mug-hot').css('color', '#8e8e93')
+      $$('.fa-glass-cheers').css('color', '#8e8e93')
+      service = 2
+    }
+    if ($$('#service-3').prop('checked')) {
+      $$('.fa-glass-cheers').css('color', '#bfcd3a')
+      $$('.fa-hamburger').css('color', '#8e8e93')
+      $$('.fa-mug-hot').css('color', '#8e8e93')
+      service = 3
+    }
+
+    // Second group of radio buttons
+    if ($$('#smile-1').prop('checked')) {
+      $$('.fa-meh-rolling-eyes').css('color', '#bfcd3a')
+      $$('.fa-smile').css('color', '#8e8e93')
+      $$('.fa-grin-stars').css('color', '#8e8e93')
+      smile = 1
+    }
+    if ($$('#smile-2').prop('checked')) {
+      $$('.fa-smile').css('color', '#bfcd3a')
+      $$('.fa-meh-rolling-eyes').css('color', '#8e8e93')
+      $$('.fa-grin-stars').css('color', '#8e8e93')
+      smile = 2
+    }
+    if ($$('#smile-3').prop('checked')) {
+      $$('.fa-grin-stars').css('color', '#bfcd3a')
+      $$('.fa-smile').css('color', '#8e8e93')
+      $$('.fa-meh-rolling-eyes').css('color', '#8e8e93')
+      smile = 3
+    }
   }
+})
+
+// Calculating algorithm
+$$(document).on('change', function () {
+  console.log('change')
 })
